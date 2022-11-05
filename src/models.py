260a36ad042109ae727e01887e1eed034596f838
ken333135 <ken.lok@eezee.com.sg>
2022-11-05 13:17:57 +0800

    add new data

--- src/models.py
+++ src/models.py
@@ -204,6 +204,24 @@ class Models:
 
     """ ENDNEW """
 
+    def dropTables(self):
+        self.executeRawSql(""" 
+        DROP TABLE IF EXISTS booking;
+
+        DROP TABLE IF EXISTS member;
+        DROP TABLE IF EXISTS admin;
+        DROP TABLE IF EXISTS facility;
+        DROP TABLE IF EXISTS booking_fee;
+        DROP TABLE IF EXISTS maintenance;
+        DROP TABLE IF EXISTS fee_schedule;
+        DROP TABLE IF EXISTS holiday;
+
+        DROP TABLE IF EXISTS assignment;
+        DROP TABLE IF EXISTS book;
+        DROP TABLE IF EXISTS professor;
+        DROP TABLE IF EXISTS student;
+         """)
+
     def createModels(self):
         self.executeRawSql(
             """CREATE TABLE IF NOT EXISTS member (
