90d245b8e4ea8bc9f07e38bccc49d392a56f69ab
ken333135 <ken.lok@eezee.com.sg>
2022-11-06 15:13:46 +0800

    chagnd ata

--- src/create.sql
+++ src/create.sql
@@ -5,15 +5,15 @@
 *******************/
 
 CREATE TABLE IF NOT EXISTS holiday (
-date DATE PRIMARY KEY,
-holiday VARCHAR(32) NOT NULL
+holiday_date DATE PRIMARY KEY,
+holiday_des VARCHAR(32) NOT NULL
 );
 
 
 CREATE TABLE IF NOT EXISTS fee_schedule (
 weekend VARCHAR(8) NOT NULL,
 holiday VARCHAR(16) NOT NULL,
-time_slot_Des VARCHAR(32) NOT NULL,
+time_slot_des VARCHAR(32) NOT NULL,
 peak VARCHAR(8) NOT NULL,
 PRIMARY KEY (weekend, holiday, time_slot_Des)
 );
@@ -28,9 +28,13 @@ equipment INT NOT NULL CHECK(equipment >= 0)
 
 
 CREATE TABLE IF NOT EXISTS booking_fee (
-sports VARCHAR(16) PRIMARY KEY,
+sports VARCHAR(16) NOT NULL,
+valid_till DATE NOT NULL,
+valid_from DATE NOT NULL,
 peak_fee INT NOT NULL CHECK(peak_fee >= 0),
 non_peak_fee INT NOT NULL CHECK(non_peak_fee >= 0)
+CHECK (valid_from < valid_till),
+PRIMARY KEY (sports, valid_till)
 );
 
 
@@ -40,31 +44,38 @@ sports VARCHAR(16) NOT NULL,
 operating_hours VARCHAR(16) NOT NULL,
 aircon CHAR(1) NOT NULL,
 indoor CHAR(1) NOT NULL,
-court_size VARCHAR(16) NOT NULL,
-FOREIGN KEY (sports) REFERENCES booking_fee(sports)
+court_size VARCHAR(16) NOT NULL
 );
 
 
-CREATE TABLE IF NOT EXISTS users (
-user_id VARCHAR(16) UNIQUE NOT NULL,
-first_name VARCHAR(64) NOT NULL,
-last_name VARCHAR(64) NOT NULL,
-email VARCHAR(64) PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS member (
+first_name TEXT NOT NULL,
+last_name TEXT NOT NULL,
+email TEXT PRIMARY KEY,
 gender VARCHAR(64) NOT NULL,
-job_title VARCHAR(64),
+job_title TEXT,
 dob DATE NOT NULL CHECK (current_date > dob),
-phone_number VARCHAR(16)
+phone_number VARCHAR(16),
+password TEXT NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS admin (
+email TEXT PRIMARY KEY,
+password TEXT NOT NULL
 );
 
 
 CREATE TABLE IF NOT EXISTS booking (
 date DATE NOT NULL,
-time_slot_Des VARCHAR(32) NOT NULL,
+time_slot_des VARCHAR(32) NOT NULL,
 facility_id VARCHAR(16) NOT NULL,
-user_id VARCHAR(16) NOT NULL,
+email TEXT NOT NULL,
 weekend VARCHAR(8) NOT NULL,
+holiday VARCHAR(32) NOT NULL,
+booking_fee INT NOT NULL,
 CONSTRAINT fk1 FOREIGN KEY (facility_id) REFERENCES facility (facility_id),
-CONSTRAINT fk2 FOREIGN KEY (user_id) REFERENCES users (user_id),
+CONSTRAINT fk2 FOREIGN KEY (email) REFERENCES member (email),
 PRIMARY KEY (date, time_slot_Des, facility_id)
 );
 
